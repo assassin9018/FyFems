c1f7f45f296b000ab815c4b645eb625bea83c5bf
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-01-14 23:47:27 +0300

    Calc fields removed at message model

--- DAL/Context/MessageConfiguration.cs
+++ DAL/Context/MessageConfiguration.cs
@@ -9,13 +9,5 @@ public class MessageConfiguration : IEntityTypeConfiguration<Message>
     public void Configure(EntityTypeBuilder<Message> builder)
     {
         const string tableName = $"\"{nameof(Message)}s\"";
-
-        builder
-            .Property(x => x.AttachmentsCount)
-            .HasComputedColumnSql<int>($"cardinality(\"{nameof(Message.Attachments)}\")", true);
-
-        builder
-            .Property(x => x.ImagesCount)
-            .HasComputedColumnSql<int>($"cardinality(\"{nameof(Message.Images)}\")", true);
     }
 }
