dd4a332ba538cbacfb5bf83cde86fd47f43731cc
assassin9018 <34675794+assassin9018@users.noreply.github.com>
2022-03-23 12:18:31 +0300

    Error handling added for wpf client

---
+++ MyFemsWpfClient/Helpers/AppLogger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using MyFems.Clients.Shared.Messages;
+using System;
+using MSLogLevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace MyFemsWpfClient.Helpers;
+
+internal class AppLogger : IAppLogger
+{
+    //Экземпляр логгера NLog
+    private readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+    public AppLogger()
+    {
+    }
+
+    public void Info(string message)
+    {
+        _logger.Info(message);
+        WeakReferenceMessenger.Default.Send(new ApplicationLog(MSLogLevel.Information, message));
+    }
+
+    public void Error(Exception ex)
+    {
+        var message = $"Source:{ex.Source}, Message:{ex.Message}\nTrace:{ex.StackTrace}";
+        WeakReferenceMessenger.Default.Send(new ApplicationLog(MSLogLevel.Error, message));
+    }
+
+    public void Error(string message, Exception? ex = null)
+    {
+        var fullMessage = ex is null ? message : $"Source:{ex.Source}, Message:{ex.Message}, Additional:{message}\nTrace:{ex.StackTrace}";
+        _logger.Error(message);
+        //Отправляем сообщение в шину
+        WeakReferenceMessenger.Default.Send(new ApplicationLog(MSLogLevel.Error, fullMessage, ex));
+    }
+}
+
+internal interface IAppLogger
+{
+    void Info(string message);
+    void Error(Exception exception);
+    void Error(string message, Exception? exception = null);
+}
---
+++ Shared/Messages/ApplicationLog.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFems.Clients.Shared.Messages;
+public class ApplicationLog
+{
+    public ApplicationLog(LogLevel information, string message)
+    {
+        Information = information;
+        Message = message;
+    }
+
+    public ApplicationLog(LogLevel information, string message, Exception? ex) : this(information, message)
+    {
+        Ex = ex;
+    }
+
+    public LogLevel Information { get; }
+    public string Message { get; }
+    public Exception? Ex { get; }
+}
